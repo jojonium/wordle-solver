90b9c59a29eda623ab07a3ae285f1cbf436c69fd
Joseph Petitti <joseph@petitti.org>
2022-02-15 08:53:25 -0500

    Update with NY Times word list

--- README.md
+++ README.md
@@ -5,9 +5,15 @@ by default. To see another day's solution provide the desired date as a
 command-line argument, e.g. `node index.js 2022-01-24`. This accepts any date
 format accepted by JavaScript's `Date()` constructor.
 
+When the New York Times acquired Wordle they changed the word list, removing six
+words from the sequence. This tool uses the New York Times word list by default,
+but you can tell it to use the original word list by including the `-o` or
+`--old` flag.
+
 Usage examples:
 ```
 $ yarn start              # Start with yarn.
 $ node index.js           # Start with node.
 $ yarn start 2022-01-24   # Show the solution for a particular date.
+$ yarn start -o           # Show solution using the original word list.
 ```
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const fs = require('fs');
 
 const main = async () => {
-  const words = (await fs.promises.readFile('word-list.txt'))
+  let source = 'ny-times-word-list.txt';
+  if (process.argv.includes('-o') || process.argv.includes('--old')) {
+    console.log('Using original powerlanguage.co.uk word list');
+    source = 'old-word-list.txt';
+  }
+  const words = (await fs.promises.readFile(source))
     .toString()
     .trim()
     .split('\n');
+  const argv = process.argv.filter((val, i) => i >= 2 && !val.startsWith('-'));
   const startDate = new Date(2021, 5, 19, 0, 0, 0, 0);
-  const targetDate = new Date(process.argv[2] || new Date());
+  const targetDate = new Date(argv[0] || new Date());
   const t = targetDate.setHours(0, 0, 0, 0) - startDate.setHours(0, 0, 0, 0);
   const wordleNum = Math.round(t / 864e5);
   console.log(`Wordle ${wordleNum}: ${words[wordleNum]}`);
